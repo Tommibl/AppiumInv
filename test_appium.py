9c96aa90a6398d9060252ce7440186c364fde3c9
Темирлан <tommibl@outlook.com>
2024-09-03 15:44:09 +0500

    Исправлен запуск теста через аппиум на эмуляторе

--- test_appium.py
+++ test_appium.py
@@ -4,26 +4,24 @@ from appium.webdriver.common.appiumby import AppiumBy
 from appium.options.android import UiAutomator2Options
 from time import sleep
 
-capabilities = dict(
-    platformName='Android',
-    automationName='uiautomator2',
-    deviceName='Android',
-    appPackage='com.android.settings',
-    appActivity='.Settings',
-    language='en',
-    locale='US'
-)
-options =  UiAutomator2Options()
-capabilities_options = options.load_capabilities(capabilities)
+capabilities_options = UiAutomator2Options().set_capability('platformName', 'Android')
+capabilities_options.set_capability('automationName', 'uiautomator2')
+capabilities_options.set_capability('deviceName', 'Android')
+capabilities_options.set_capability('appPackage', 'com.android.settings')
+capabilities_options.set_capability('appActivity', '.Settings')
+capabilities_options.set_capability('language', 'en')
+capabilities_options.set_capability('locale', 'US')
 
-appium_server_url = 'http://localhost:4723'
+#appium_server_url = 'http://localhost:4723/wd/hub'
+appium_server_url = 'http://127.0.0.1:4723'
+
 
 @pytest.fixture()
 def driver():
-    app_driver = webdriver.Remote(appium_server_url, capabilities_options)
+    app_driver = webdriver.Remote(appium_server_url, options = capabilities_options)
     yield app_driver
-    if driver:
-        driver.quit()
+    if app_driver:
+        app_driver.quit()
 
 def test_find_battery(driver) -> None:
     el = driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
